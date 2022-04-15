5b069440432bd1dc63c582976383728ebef0ae45
Sara Song <shsong@andrew.cmu.edu>
2022-04-15 17:12:04 -0400

    fixed typos, added spacing in code

--- bouncr/app/models/invite.rb
+++ bouncr/app/models/invite.rb
@@ -1,8 +1,9 @@
 class Invite < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  
   #Scope
-  scope :by_user, -> (user_id){where('user_id = ?',user_id)}
-  scope :by_event, ->(event_id){where('event_id = ?',event_id)}
-  scope :checkedIn, ->{where('checkinStatus = ?',true)}
+  scope :by_user, -> (user_id) { where('user_id = ?',user_id) }
+  scope :by_event, -> (event_id) { where('event_id = ?',event_id) }
+  scope :checkedIn, -> { where('checkinStatus = ?',true) }
 end
