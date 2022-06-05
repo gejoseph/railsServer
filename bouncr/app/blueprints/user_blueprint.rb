55255b46f1c0a1fdd6f5c6d5fbacdd3aa33d5326
Sara Song <shsong@andrew.cmu.edu>
2022-06-05 15:24:47 -0400

    changed rails v to 2.6.9

--- bouncr/app/blueprints/user_blueprint.rb
+++ bouncr/app/blueprints/user_blueprint.rb
@@ -9,6 +9,8 @@ class UserBlueprint < Blueprinter::Base
     fields :username, :firstName, :lastName
   end
 
+  # Add new view w/ token input
+
   view :login do
     #fields :username, :email, :firstName, :lastName, :phoneNumber, :birthday
     fields (:token) do |_user, options|
